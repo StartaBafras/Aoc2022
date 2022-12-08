1941b6c9c4411aae3e2af1bc8b6635d4afa9e893
StartaBafras <beyhankaradag00@gmail.com>
2022-12-08 19:03:24 +0300

    Fonksiyonel hale getirildi

--- D5/day5_1.c
+++ D5/day5_1.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 
 
 #define STACK_SIZE 10
+#define INPUT_SIZE 40
+#define MAX_STACK 9
 
 typedef struct stack
 {
@@ -30,7 +33,7 @@ char push(stack *stack_value, char value)
     return 0;
 }
 
-stack *stack_gen()
+stack *stack_generator()
 {
     stack *new_stack = malloc(sizeof(stack));
     new_stack->pop = &pop;
@@ -40,20 +43,66 @@ stack *stack_gen()
     return new_stack;
 }
 
+int read_supply(char *file_name,stack *stack_array[])
+{
+    FILE *file = fopen("d5.txt","r");
+    char input[INPUT_SIZE];
+    for(int i=0; i<10; i++)
+    {
+        //fscanf(file,"%[^\n]",input);
+        fgets(input,40,file);
+
+        for(int j=0; j<INPUT_SIZE; j++)
+        {
+            if(input[j] <= 90 && input[j] >= 65)
+            {
+                stack_array[j/4]->push(stack_array[j/4],input[j]);
+            }
+        }
+        memset(input,0,INPUT_SIZE);
+    }
+
+    fclose(file);
+
+    return 0;
+
+}
+
+int reverse_stack(stack *stack_array[])
+{
+    char temp = 0;
+    for(int i=0; i<MAX_STACK; i++)
+    {
+        for(int j=stack_array[i]->stack_level,k=0; strlen(stack_array[i]->stack)/2 > k; j--,k++)
+        {
+            temp = stack_array[i]->stack[j];
+            stack_array[i]->stack[j] = stack_array[i]->stack[k];
+            stack_array[i]->stack[k] = temp;
+        }
+        
+    }
+}
+
 int main(void)
 {
+    stack *stack_array[MAX_STACK];
 
+    for(int i=0; i<MAX_STACK; i++)
+    {
+        stack_array[i] = stack_generator();
+    }
 
-    stack *a = stack_gen();
+    read_supply("d5.txt",stack_array);
+    reverse_stack(stack_array);
 
-    a->push(a,'c');
-    a->push(a,'d');
-    //push(&a,'c');
-    push(a,'b');
+    FILE *file = fopen("d5.txt","r");
+    while (!feof(file))
+    {
+        for(int i=0; i<10; i++) fgets(stdout,40,file);
 
-    printf("%c",pop(a));
-    printf("%c",pop(a));
-    printf("%c",a->pop(a));
+        
+    }
+    
 
     return 0;
 }
